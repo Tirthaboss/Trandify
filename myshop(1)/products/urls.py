094831434b0ca9b2bb247e4be2fa90680e826051
Tirthaboss <167761932+Tirthaboss@users.noreply.github.com>
2024-10-23 17:36:42 +0530

    Update urls.py

--- myshop(1)/products/urls.py
+++ myshop(1)/products/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import product_list
 
 urlpatterns = [
-    path('', product_list, name='product_list'),
+    path('shop/', product_list, name='product_list'),
 ]
