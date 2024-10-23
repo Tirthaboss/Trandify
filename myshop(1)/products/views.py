812a53c20597256a920fbbc73ceac2a55cd4d5d0
Tirthaboss <167761932+Tirthaboss@users.noreply.github.com>
2024-10-23 17:32:58 +0530

    Update views.py

--- myshop(1)/products/views.py
+++ myshop(1)/products/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
-   from django.shortcuts import render
-   from .models import Product
-
-   def product_list(request):
-       products = Product.objects.all()
-       return render(request, 'products/product_list.html', {'products': products})
-   
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'products/product_list.html', {'products': products})
